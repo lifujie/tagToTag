d962c0eb9670336dd6461dce2c2ffa8bd71ebba3
mac <mac@l00277880.local>
2018-10-18 17:27:23 +0800

    Modify feature: 添加阿里仓库

--- main.go
+++ main.go
@@ -11,9 +11,35 @@ import (
 )
 
 func execShellCmd(cmd string) (string, error) {
+	fmt.Printf("cmd: %s\n", cmd)
 	result, err := exec.Command("/bin/sh", "-c", cmd).Output()
 	return string(result), err
 }
+
+func pullAndTurnTag(turned, whichImage string) error {
+	cmdPath, err := exec.LookPath("docker")
+	if err != nil {
+		log.Fatal("docker is no install\n")
+	}
+
+	cmd := fmt.Sprintf("%s %s %s", cmdPath, "pull", turned)
+	_, err = execShellCmd(cmd)
+	if err != nil {
+		return err
+	}
+
+	//tag改名
+	cmd = fmt.Sprintf("%s %s %s %s", cmdPath, "tag", turned, whichImage)
+	_, err = execShellCmd(cmd)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+var (
+	registryList = map[string]func(string) string{"ali": pathToPathAli, "anjia": pathToPathAnJia}
+)
 
 func doSomething(whichImage string) {
 	// var status = map[string]string{
@@ -23,26 +49,17 @@ func doSomething(whichImage string) {
 	// 	"130": "(Ctrl+C)终止",
 	// }
 
-	cmdPath, err := exec.LookPath("docker")
-	if err != nil {
-		log.Fatal("docker is no install\n")
-	}
-
 	//转换路径，下载
-	turned := pathToPath(whichImage)
-	cmd := fmt.Sprintf("%s %s %s", cmdPath, "pull", turned)
-	_, err = execShellCmd(cmd)
-	if err != nil {
-		fmt.Printf("Error: %s\n", err.Error())
+	var err error
+	var turned string
+	for k, v := range registryList {
+		turned = v(whichImage)
+		err = pullAndTurnTag(turned, whichImage)
+		if err == nil {
+			fmt.Printf("From registry %s Get %s\n", k, whichImage)
+			break
+		}
 	}
-
-	//tag改名
-	cmd = fmt.Sprintf("%s %s %s %s", cmdPath, "tag", turned, whichImage)
-	_, err = execShellCmd(cmd)
-	if err != nil {
-		fmt.Printf("Error: %s\n", err.Error())
-	}
-
 	//fmt.Println(result, status)
 }
 
@@ -55,7 +72,7 @@ docker pull gcr.io/google-containers/federation-controller-manager-arm64:v1.3.1-
 # eq
 docker pull k8s.gcr.io/federation-controller-manager-arm64:v1.3.1-beta.1
 */
-func pathToPath(path string) string {
+func pathToPathAnJia(path string) string {
 	var turned string
 
 	pathSplit := strings.Split(path, "/")
@@ -65,6 +82,27 @@ func pathToPath(path string) string {
 		turned = "anjia0532" + "/" + pathSplit[1] + "." + pathSplit[2]
 	case "k8s.gcr.io":
 		turned = "anjia0532" + "/" + "google-containers" + "." + pathSplit[1]
+	}
+
+	return turned
+}
+
+// # 下载阿里云镜像
+// docker pull registry.cn-hangzhou.aliyuncs.com/google-containers/pause-amd64:3.0
+
+// # 本地命名为 gcr.io/google_containers/pause-amd64:3.0
+// docker tag registry.cn-hangzhou.aliyuncs.com/google-containers/pause-amd64:3.0 gcr.io/google_containers/pause-amd64:3.0
+//
+func pathToPathAli(path string) string {
+	var turned string
+
+	pathSplit := strings.Split(path, "/")
+
+	switch pathSplit[0] {
+	case "gcr.io":
+		turned = "registry.cn-hangzhou.aliyuncs.com" + "/" + pathSplit[1] + "/" + pathSplit[2]
+	default:
+		fmt.Printf("ali not reachable\n")
 	}
 
 	return turned
